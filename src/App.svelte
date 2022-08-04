cd9f8adaf04409d4802bcd247c837d09a2af8b84
EliasDerHai <eliashaider95@gmail.om>
2022-08-05 00:27:09 +0200

    add toasts

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Todo } from "./models/todo";
 	import TodoList from "./components/TodoList.svelte";
+	import "smelte/src/tailwind.css";
 
 	const todos: Todo[] = [
 		new Todo("hear about svelte"),
@@ -8,8 +9,12 @@
 		new Todo("get bored"),
 		new Todo("check out sth else..."),
 	];
-	
 </script>
+
+<link
+	href="https://fonts.googleapis.com/css?family=Roboto:300,400,500|Material+Icons&display=swap"
+	rel="stylesheet"
+/>
 
 <main>
 	<TodoList {todos} />
--- src/components/TodoItem.svelte
+++ src/components/TodoItem.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
     import type { Todo } from "../models/todo";
+    import Toasts from "./toast/Toasts.svelte";
+    import { addToast } from "./toast/store";
 
     export let todo: Todo;
+
+    const toast = () => {
+        const message = todo.done
+            ? `You finished ${todo.description} - Great job!`
+            : "Not yet done?";
+        addToast({ message, timeout: 750 });
+    };
 </script>
 
+<Toasts />
+
 <li>
-    <input
-        type="checkbox"
-        bind:checked={todo.done}
-        on:change={() => console.log(todo)}
-    />
+    <input type="checkbox" bind:checked={todo.done} on:change={toast} />
     <span class:done={todo.done}>{todo.description}</span>
 </li>
 
